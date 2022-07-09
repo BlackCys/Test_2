28ca5540dded3d09348c048dc8d00f263a4024d9
HikigayaAkuro <hikigaya.akuro@gmail.com>
2022-07-09 22:55:33 +0700

    Add commit

--- src/test/first/test1.java
+++ src/test/first/test1.java
@@ -4,5 +4,6 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World 1");
 		System.out.println("Hello World 2");
+		System.out.println("Hello World 3");
 	}
 }
